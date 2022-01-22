b62390ccc5d87f64383b1a05ea9e1df3a9b3add4
kyledeveloper <kerwinliang6@gmail.com>
2022-01-21 18:48:59 -0800

    update

---
+++ project/F1.py
@@ -0,0 +1,7 @@
+class Features1:
+    def __init__(self) -> None:
+        self.intellenge = 0
+        self.force = 0
+        self.physicalPower = 0
+        self.luck = 0
+        self.charm = 0
--- project/people.py
+++ project/people.py
@@ -1,7 +1,16 @@
-class people:
+
+from project.F1 import Features1
+
+
+class People:
     def __init__(self,name,age = 18,star = 1,sex = 0) -> None:
         self.name = name
         self.currentage = age
         self.sex = 0
         self.star = star
-        self.lv = 1
+        self.personalFeature = Features1
+
+
+if __name__ == '__main__':
+    myself = People("KyleLiang", age = 28,star = 1,sex = 1)
+    print(myself.personalFeature.charm)
